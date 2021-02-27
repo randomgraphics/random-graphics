a04c2d22d39624961249ec9a69ccaf9370c3a33a
randomgraphics <gfxrgb@gmail.com>
2021-02-27 10:34:16 -0800

    wrote log to android logcat

--- src/01-base/log.cpp
+++ src/01-base/log.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #if RG_MSWIN
 #include <windows.h>
+#elif RG_ANDROID
+#include <android/log.h>
 #endif
 
 using namespace rg;
@@ -27,11 +29,39 @@ static inline std::string sev2str(int sev) {
 
 // ---------------------------------------------------------------------------------------------------------------------
 //
-static void writeToDebuggerOutput(const std::string & messageWithNewLine) {
+static void writeToSystemLog(int severity, const std::string & messageWithNewLine) {
 #if RG_MSWIN
+    (void)severity;
     ::OutputDebugStringA(messageWithNewLine.c_str());
+#elif RG_ANDROID
+    int priority;
+    if (severity <= rg::log::macros::F) {
+        priority = ANDROID_LOG_FATAL;
+    } else if (severity <= rg::log::macros::E) {
+        priority = ANDROID_LOG_ERROR;
+    } else if (severity <= rg::log::macros::W) {
+        priority = ANDROID_LOG_WARN;
+    } else if (severity <= rg::log::macros::I) {
+        priority = ANDROID_LOG_INFO;
+    } else {
+        priority = ANDROID_LOG_VERBOSE;
+    }
+    {
+        std::istringstream text(messageWithNewLine);
+        static std::mutex m;
+        std::lock_guard<std::mutex> guard(m); // this is to prevent log from different threads to get interleaved with each other.
+        // static std::string logFileName = getLogFileName();
+        // std::ofstream f(logFileName, std::ios_base::app);
+        // auto prefix = getLogPrefix();
+        std::string l;
+        while (std::getline(text, l)) {
+            __android_log_print(priority, "RandomG", "%s", l.c_str());
+            // f << prefix << tag << l << std::endl;
+        }
+    }
 #else
     // GDB/LLDB ?
+    (void)severity;
     (void)messageWithNewLine;
 #endif
 }
@@ -40,16 +70,26 @@ static void writeToDebuggerOutput(const std::string & messageWithNewLine) {
 //
 static void defaultLogCallback(void *, const LogDesc & desc, const char * text) {
     if (!text || !*text) return;
+    
+    std::stringstream ss;
+    FILE * fp;
     if (desc.severity >= rg::log::macros::I) {
+        ss << "[" << sev2str(desc.severity) << "] " << text << std::endl;
+        fp = stdout;
         fprintf(stdout, "[%s] %s\n", sev2str(desc.severity).c_str(),  text);
     } else {
-        std::stringstream ss;
         ss << "[" << sev2str(desc.severity) << "] " << desc.file << ":" << desc.line << " - " << text << std::endl;
-        auto str = ss.str();
-        fprintf(stderr, str.c_str());
-        writeToDebuggerOutput(str);
+        fp = stderr;
     }
+
+    auto str = ss.str();
+    fprintf(fp, "%s", str.c_str());
+    
+    writeToSystemLog(desc.severity, str);
 }
+
+// ---------------------------------------------------------------------------------------------------------------------
+//
 RG_API std::atomic<LogCallback> globalLogCallback = { { defaultLogCallback, nullptr } };
 RG_API LogCallback rg::setLogCallback(LogCallback lc) {
     if (!lc.func) lc.func = defaultLogCallback;
