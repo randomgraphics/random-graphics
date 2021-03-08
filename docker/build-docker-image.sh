fc6771e3e1059bd29a95175a214e9a639fc5b9e0
Chen Li <chen.li@innopeaktech.com>
2021-03-07 22:52:34 -0800

    added a script to build docker image

---
+++ docker/build-docker-image.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+docker build -t randomgraphics/vulkan:1.2.162 $@ -<<EOF
+FROM ubuntu:20.04
+
+LABEL description="random-graphics-vulkan"
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update
+RUN apt-get install -y wget gnupg2
+RUN wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
+RUN wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.162-focal.list https://packages.lunarg.com/vulkan/1.2.162/lunarg-vulkan-1.2.162-focal.list
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    git \
+    git-lfs \
+    ninja-build \
+    vulkan-sdk \
+    libglfw3-dev \
+    libxinerama-dev \
+    libxcursor-dev \
+    libxi-dev
+EOF
+
+docker build -t randomgraphics/vulkan-android $@ -<<EOF
+FROM randomgraphics/vulkan:1.2.162
+
+# download android studio (maybe not needed?)
+RUN wget -qO /opt/android-studio-ide-201.7042882-linux.tar.gz https://redirector.gvt1.com/edgedl/android/studio/ide-zips/4.1.2.0/android-studio-ide-201.7042882-linux.tar.gz
+RUN cd /opt;tar xzf android-studio-ide-201.7042882-linux.tar.gz
+RUN mkdir /opt/android;mv /opt/android-studio /opt/android/studio
+RUN rm /opt/android-studio-ide-201.7042882-linux.tar.gz
+
+# download sdk tools
+RUN apt-get install unzip
+RUN cd /opt;wget -q https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+RUN cd /opt;unzip commandlinetools-linux-6858069_latest.zip
+RUN mkdir -p /opt/android/sdk/cmdline-tools
+RUN mv /opt/cmdline-tools /opt/android/sdk/cmdline-tools/latest
+
+# download sdk components
+ARG JAVA_HOME=/opt/android/studio/jre
+RUN yes|/opt/android/sdk/cmdline-tools/latest/bin/sdkmanager --licenses
+RUN /opt/android/sdk/cmdline-tools/latest/bin/sdkmanager --install "ndk;22.0.7026061"
+RUN /opt/android/sdk/cmdline-tools/latest/bin/sdkmanager --install "build-tools;30.0.3"
+RUN /opt/android/sdk/cmdline-tools/latest/bin/sdkmanager --install "platforms;android-30"
+RUN /opt/android/sdk/cmdline-tools/latest/bin/sdkmanager --install "cmake;3.10.2.4988404"
+EOF
