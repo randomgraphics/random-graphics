80232edd4f9c66ca2b918ba69a8f94bddd328dfc
Chen Li <chen.li@innopeaktech.com>
2021-07-13 16:59:38 -0700

    updated docker tag

---
+++ docker/pull-from-docker-hub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker pull randomgraphics/vulkan:1.2.182-ndk-22.1.7171670-cuda-11.3.1-ubuntu-20.04
+docker pull randomgraphics/vulkan-android:1.2.182-ndk-22.1.7171670-cuda-11.3.1-ubuntu-20.04
